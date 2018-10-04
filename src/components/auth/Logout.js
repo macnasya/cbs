d67e3fd7746a77c2cab1201ce992bfb6992d504d
“gereXciTer” <vladimir.gartsev@gmail.com>
2018-10-04 08:59:22 -0400

    fixed iOS drawer navigation issue

--- src/components/auth/Logout.js
+++ src/components/auth/Logout.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import styles from '../styles'
 import { connect } from 'react-redux'
+import { View } from 'react-native'
+import { Container, Text, Button, Content } from 'native-base';
+import UserHeader from '../user//UserHeader'
 import { logout } from '../../actions'
 
 class Logout extends React.PureComponent {
-  componentDidMount () {
-    this.props.logout()
-  }
   render () {
-    return null
+    return <Container style={styles.container}>
+    <Content>
+      <UserHeader {...this.props} title="Logout"/>
+      <Text>Are you sure you want to sign out, {this.props.user.profile && this.props.user.profile.displayName}?</Text>
+      <View style={{flexDirection: "row"}}>
+        <Button warning onPress={() => this.props.logout()}><Text>Yes</Text></Button>
+        <Button transparent={true} onPress={() => this.props.navigation.goBack()}>
+          <Text>No, keep me signed in</Text>
+        </Button>
+      </View>
+    </Content>
+  </Container>
   }
 }
 
-export default connect(() => ({}), {logout})(Logout)
+export default connect(({user}) => ({user}), {logout})(Logout)
--- src/components/user/UserDashboard.js
+++ src/components/user/UserDashboard.js
@@ -3,6 +3,7 @@ import styles from '../styles'
 import { connect } from 'react-redux'
 import { Container, Text, Button, Content } from 'native-base';
 import UserHeader from './UserHeader'
+import { logout } from '../../actions'
 
 class UserDashboard extends React.Component {
 
@@ -13,6 +14,7 @@ class UserDashboard extends React.Component {
           <UserHeader {...this.props} title="Dashboard"/>
           <Text>Welcome back,</Text>
           <Text>{this.props.user.profile && this.props.user.profile.displayName}</Text>
+          <Button onPress={() => this.props.navigation.navigate('Logout')}><Text>Logout</Text></Button>
         </Content>
       </Container>
     );
@@ -20,4 +22,4 @@ class UserDashboard extends React.Component {
 
 }
 
-export default connect(({user}) => ({user}))(UserDashboard)
+export default connect(({user}) => ({user}), {logout})(UserDashboard)
